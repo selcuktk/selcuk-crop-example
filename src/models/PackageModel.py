ffa8f16da1e037564954d6e40841b25ab65f1182
Selçuk Talha Kul <157029095+selcuktk@users.noreply.github.com>
2025-07-08 18:13:01 +0300

    Update PackageModel.py
    feat: PackageModel fix 4

--- src/models/PackageModel.py
+++ src/models/PackageModel.py
@@ -94,7 +94,7 @@ class SelcukCropExampleExecutorOutputs(Outputs):
 
 class SelcukCropExampleExecutorRequest(Request):
     inputs: Optional[SelcukCropExampleExecutorInputs]
-    configs: PackageConfigs
+    configs: SelcukCropExampleExecutorConfigs
 
     class Config:
         json_schema_extra = {
